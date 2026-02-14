17fc9d693aa75c71f2d9759ae991419b702e09cc
Stephane <stephane@local>
2026-02-14 22:42:02 +0100

    v0.22: Back to Anton original layout (6x8 x2, date+day on 2 lines) at y+52

--- apps/stephclk/app.js
+++ apps/stephclk/app.js
@@ -17,14 +17,11 @@ let draw = function() {
   var timeStr = require("locale").time(date, 1); // Hour and minute
   g.setFontAlign(0, 0).setFont("Anton").drawString(timeStr, x, y);
 
-  // Show date and day of week in French
+  // Show date and day of week in French (Anton original layout, slightly lower)
   var mois = ["JAN","FEV","MAR","AVR","MAI","JUN","JUL","AOU","SEP","OCT","NOV","DEC"];
   var jours = ["DIMANCHE","LUNDI","MARDI","MERCREDI","JEUDI","VENDREDI","SAMEDI"];
-  // Date higher up
-  var dateStr = date.getDate()+" "+mois[date.getMonth()]+" "+date.getFullYear();
-  g.setFontAlign(0, 0).setFont("6x8", 2).drawString(dateStr, x, y+38);
-  // Day of week - keep same position but make it bigger
-  g.setFontAlign(0, 0).setFont("12x20", 3).drawString(jours[date.getDay()], x, y+65);
+  var dateStr = date.getDate()+" "+mois[date.getMonth()]+" "+date.getFullYear()+"\n"+jours[date.getDay()];
+  g.setFontAlign(0, 0).setFont("6x8", 2).drawString(dateStr, x, y+52);
 
   // queue next draw
   if (drawTimeout) clearTimeout(drawTimeout);
